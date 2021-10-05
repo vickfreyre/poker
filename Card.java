22992734fc988d66839de93390f533ea1637edf3
Victor Freyre <vickfreyre@gmail.com>
2021-10-04 23:33:10 -0500

    Segundo commit

--- Card.java
+++ Card.java
@@ -1,51 +1,29 @@
-import java.util.HashSet;
-import java.util.Iterator;
-
 class Card {
+    String palo;
+    String color;
+    String valor;
 
-    Card palo = new Card();
-    Card color = new Card();
-    Card valor = new Card();
+    // Creación los métodos con getters y setters que componen la clase carta
+    Card(String palo, String color, String valor){
+        this.palo = palo;
+        this.color = color;
+        this.valor =  valor;
+    }
+    @Override
+    public String toString(){
 
-    public static void main(String[] args) {
-
-        HashSet palo = new HashSet();
-        palo.add("Treboles");
-        palo.add("Corazones");
-        palo.add("Picas");
-        palo.add("Diamantes");
-
-        HashSet<String> color = new HashSet<String>();
-        color.add("Rojo");
-        color.add("Negro");
-
-        HashSet valor = new HashSet();
-        valor.add(2);
-        valor.add(3);
-        valor.add(4);
-        valor.add(5);
-        valor.add(6);
-        valor.add(7);
-        valor.add(8);
-        valor.add(9);
-        valor.add(10);
-        valor.add("J");
-        valor.add("Q");
-        valor.add("K");
-        valor.add("A");
-
-
-        Object[] arr = palo.toArray();
-        for (int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
+        // Si el palo es corazón o diamante, mediante una condición lo establecemos en rojo
+        if(palo == "Corazones" || palo == "Diamantes") {
+            color = "Rojo";
         }
 
+        // Si el palo no es corazón o diamante, entonces el color del palo será negro (cuando son tréboles o picas)
+        else
+            color = "Negro";
 
+        return palo + ", " + color + ", " + valor;
     }
-
 }
-
-
 
 
 
