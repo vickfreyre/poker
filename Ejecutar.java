e41a02f71cd97aea63c1a1d7346ce2e743878c6b
Victor Freyre <vickfreyre@gmail.com>
2021-10-04 23:43:15 -0500

    Segundo commit

--- Ejecutar.java
+++ Ejecutar.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Ejecutar {
+// En la clase Ejecutar se coloca el método main.
+
+class Ejecutar {
+    public static void main(String[] args) {
+
+        //Objeto cartas instanciado de la clase Deck y sus métodos.
+        Deck cartas = new Deck();
+
+        //Mostrar la indicación de cartas mezcladas.
+        System.out.println();
+        System.out.println("*** SHUFFLE ***");
+        cartas.shuffle();
+
+        //Visualización de la primera carta del mazo.
+        System.out.println();
+        System.out.println("*** HEAD ***");
+        cartas.head();
+
+        //Visualización de una carta tomada al azar de entre el mazo.
+        System.out.println();
+        System.out.println("*** PICK ***");
+        cartas.pick();
+
+        //Visualización del reparto de 5 cartas
+        System.out.println();
+        System.out.println("*** HAND ***");
+        cartas.hand();
+
+    }
 }
